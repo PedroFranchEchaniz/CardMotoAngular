f81e844b31d67b48e27fbde48eaeb35675591269
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-10-11 14:16:02 +0200

    carAngular

---
+++ src/app/login-screem/login-screem.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-screem',
+  templateUrl: './login-screem.component.html',
+  styleUrls: ['./login-screem.component.css']
+})
+export class LoginScreemComponent {
+
+}
